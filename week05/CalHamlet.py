b128a37c9dfa9725af93cf564942e7ba4491c4f7
star <18792146966@163.com>
2018-06-27 22:45:02 +0800

    统计hamlet常用词组&&《三国演义》中的人物出场顺序

---
+++ week05/CalHamlet.py
@@ -0,0 +1,18 @@
+#CalHamletv1.py
+def  getText():
+	txt = open("hamlet.txt",'r').read()
+	txt = txt.lower()
+	for ch in '~@!#$%^&*()_+[/]{;:}"/\\<=‘’.->*|`,?':
+		txt = txt.replace(ch,' ')
+	return txt
+hamlet_txt = getText()
+words = hamlet_txt.split()
+counts = {}
+for word in words:
+	counts[word] = counts.get(word,0)+1
+items = list(counts.items())
+items.sort(key = lambda x:x[1], reverse=True)
+for i in range(10):
+	word,count = items[i]
+	print('{0:<10}{1:>5}'.format(word,count))
+
