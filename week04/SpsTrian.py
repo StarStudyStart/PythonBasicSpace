57e3167d494657405a3acbc2775ed66e64c3a2a3
star <18792146966@163.com>
2018-06-28 23:09:20 +0800

    test

---
+++ week04/SpsTrian.py
@@ -0,0 +1,15 @@
+#SksTaian.py
+import turtle
+def triangleDraw(len,x=0,y=0):
+	turtle.goto(x,y)
+	turtle.fd(len)
+	turtle.right(120)
+	turtle.fd(len)
+	turtle.left(120)
+	turtle.fd(len)
+def SpsDraw(len,level):
+	if  level == 0:
+		triangleDraw(len)
+	else :
+		for 
+triangleDraw(400)
